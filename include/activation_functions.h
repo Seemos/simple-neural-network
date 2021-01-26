e5b068220d661b171468cae9087ed8896a483576
simon schlude <simon@schlu.de>
2021-01-26 20:51:59 +0100

    added tanh and ReLu activation functions

--- include/activation_functions.h
+++ include/activation_functions.h
@@ -4,10 +4,21 @@
 #include <cmath>
 #include <utility>
 
+const double EULER     = std::exp(1.0);
 const unsigned SIGMOID = 0;
+const unsigned TANH    = 1;
+const unsigned RELU    = 2;
 
 long double sigmoid(long double x);
 long double sigmoid_derivative(long double x);
+
+long double softsign(long double x);
+long double softsign_derivative(long double x);
+
+long double tanh_derivative(long double x);
+
+long double relu(long double x);
+long double relu_derivative(long double x);
 
 extern long double (*activations[])(long double);
 extern long double (*derivatives[])(long double);
--- src/activation_functions.cpp
+++ src/activation_functions.cpp
@@ -1,12 +1,26 @@
 #include<cmath>
 #include "activation_functions.h"
 
+// SIGMOID
 long double sigmoid(long double x) {
-	return 1 / (1 + pow(std::exp(1.0), -x));
+	return 1 / (1 + pow(EULER, -x));
 }
 long double sigmoid_derivative(long double x) {
 	return sigmoid(x) * (1 - sigmoid(x));
 }
 
-long double (*activations[])(long double) = {sigmoid};
-long double (*derivatives[])(long double) = {sigmoid_derivative};
+// TANH
+long double tanh_derivative(long double x){
+	return 1 - pow(tanhl(x), 2);
+}
+
+// RELU
+long double relu(long double x){
+	return x > 0 ? x : 0;
+}
+long double relu_derivative(long double x){
+	return x > 0 ? 1 : 0;
+}
+
+long double (*activations[])(long double) = {sigmoid, tanhl, relu};
+long double (*derivatives[])(long double) = {sigmoid_derivative, tanh_derivative, relu_derivative};
