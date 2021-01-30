62ab6bd4dfd962fd906946a2d3094fb28b7d9c30
simon schlude <simon@schlu.de>
2021-01-30 21:44:18 +0100

    implemented add_hidden_layer function

--- src/layered_network.cpp
+++ src/layered_network.cpp
@@ -9,3 +9,9 @@ layered_network create_layered_network(unsigned n_inputs, unsigned n_outputs, un
     net.f_outputs = activation;
     return net;
 }
+
+void add_hidden_layer(layered_network& net, unsigned n_neurons, unsigned activation){
+    net.is_built = false;
+    net.topology_neurons.push_back(n_neurons);
+    net.topology_functions.push_back(activation);
+}
