ee2e9ed977c9ebea2d48d3abcf202ecb38c35083
simon schlude <simon@schlu.de>
2021-02-02 19:31:07 +0100

    updated main.cpp to test layerd_network functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,28 @@
 #include"matrix.h"
 #include"activation_functions.h"
 #include"neural_network.h"
+#include"layered_network.h"
 
 int main() {
 	matrix mat_input = create_matrix_from_vector(read_csv("./data/input.csv"));
 	matrix mat_target = create_matrix_from_vector(read_csv("./data/output.csv"));
 	matrix result;
 
-	neural_network net = create_neural_network(3, 4, 1, SIGMOID, SIGMOID);
-	printf("The untrained predictions:\n");
-	result = query_neural_network(net, mat_input);
-	print_matrix(result);
+	layered_network net = create_layered_network(3,1, SIGMOID);
+	add_hidden_layer(net, 4, SIGMOID);
+	build_layered_network(net);
 
-	printf("Starting training...\n");
-	for (unsigned i = 0; i < 1000; i++) {
-		train_neural_network(net, mat_input, mat_target, 1);
+	for (unsigned i = 0; i < 100; i++)
+	{
+		train_layered_network(net, mat_input, mat_target, 0.1);
 	}
 
-	printf("The values should be 0,0,1,1\n");
-	result = query_neural_network(net, mat_input);
-	print_matrix(result);
-
-	printf("Saving the net to a file\n");
-	save_neural_network(net, "net.dat");
-
-	printf("Create a copy of the net from a file\n");
-	neural_network net_loaded = load_neural_network("net.dat");
-
-	printf("Query copied net\n");
-	print_matrix(query_neural_network(net, mat_input));
+	print_matrix(query_layered_network(net, mat_input));
+	
+	printf("----------Input------------");
+	print_matrix(mat_input);
+	printf("----------Target-----------");
+	print_matrix(mat_target);
+	printf("----------Result-----------");
+	print_matrix(query_layered_network(net, mat_input));
 }
